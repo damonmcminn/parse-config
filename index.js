87bcdb104782a4df692d99c5a0a99ed35e799e72
Damon McMinn <damonrmcminn@gmail.com>
2015-04-16 13:18:01 +0100

    ES5 build

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-import {readFileSync as read} from 'fs';
+'use strict';
 
-const HOME = process.env.HOME;
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
 
-export default (function(args) {
+var _read = require('fs');
 
-  let path = args.filter(arg => arg.match(/--config=/i))
-    .map(arg => { 
-      let path = arg.replace('--config=', '');
-        if (path[0] === '~') {
-          path = path.replace('~', HOME);
-        }
-      return path;
-    })
-    .pop()
+var HOME = process.env.HOME;
+
+exports['default'] = (function (args) {
+
+  var path = args.filter(function (arg) {
+    return arg.match(/--config=/i);
+  }).map(function (arg) {
+    var path = arg.replace('--config=', '');
+    if (path[0] === '~') {
+      path = path.replace('~', HOME);
+    }
+    return path;
+  }).pop();
 
   try {
-    return JSON.parse(read(path).toString());
+    return JSON.parse(_read.readFileSync(path).toString());
   } catch (e) {
-    throw new Error(`Could not be parse: ${path}`);
+    throw new Error('Could not be parse: ' + path);
   }
 })(process.argv);
+
+module.exports = exports['default'];
